21e9bc57009f589557a8e10c1177f2d0bdb42a18
Brent Stradling <brent.stradling@gmail.com>
2016-07-06 17:40:07 -0500

    attempting to make the client info modal open when selecting file>new client. Will work on more once I get home

--- templateselectorfunctions.py
+++ templateselectorfunctions.py
@@ -11,23 +11,8 @@ class templateselectorgui(Ui_MainWindow, Ui_ClientInfoModal_2):
         self.actionAdd_Client.triggered.connect(self.clientinfomodalgui)
         self.HeroImageUploadBtn.clicked.connect(self.addClient)
 
-    def addClient(self, templateselectorgui):
+    def addClient(self):
         print('hello')
-
-    class clientinfomodalgui(Ui_ClientInfoModal_2):
-        def __init__(self, dialog):
-            Ui_ClientInfoModal_2.__init__(self)
-            self.setupUi(dialog)
-            self.SaveBtn.clicked.connect(self.addClientInfo)
-            self.CancelBtn.clicked.connect(sys.exit)
-            self.data = "data.json"
-            self.clientinfo = {}
-            app = QtWidgets.QApplication(sys.argv)
-            dialog = QtWidgets.QDialog()
-            prog = clientinfomodalgui(dialog)
-            dialog.show()
-            sys.exit(app.exec_())
-
 
 
 if __name__ == "__main__":
