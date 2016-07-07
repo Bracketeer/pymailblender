84e05719b82c95c2c5ff4440da81bc83b80b7186
Bracketeer <brent.stradling@gmail.com>
2016-07-07 02:16:35 -0500

    Doing a redesign now that I understand how the stacked widget works....

---
+++ functions.py
@@ -0,0 +1,115 @@
+import sys
+import json
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from mailblender import Ui_MailBlender
+
+class mailblenderGui(Ui_MailBlender):
+
+    def __init__(self, MailBlender):
+
+        Ui_MailBlender.__init__(self)
+        self.setupUi(MailBlender)
+        self.SaveBtn.clicked.connect(self.addClientInfo)
+        self.CancelBtn.clicked.connect(sys.exit)
+        self.data = "data.json"
+        self.clientinfo = {}
+
+
+        try:
+            fileObject = open(self.data, "r")
+            self.clientinfo = json.load(fileObject)
+            fileObject.close()
+
+        # If no data file is found, return empty dictionary
+        except:
+            self.clientinfo = {}
+
+    def write(self):
+        with open('data.json', mode='a', encoding='utf-8') as fileObject:
+            json.dump(self.clientinfo, fileObject)
+        fileObject.close()
+
+    # Read Serialized file
+    def read(self, data):
+        try:
+            fileObject = open(self.data, "r")
+            self.clientinfo = json.load(fileObject)
+            fileObject.close()
+
+        # If no data file is found, return empty dictionary
+        except FileNotFoundError:
+            self.clientinfo = {}
+
+    def phoneFormat(self, p):
+        return format(int(p[:-1]), ",").replace(",", "-") + p[-1]
+
+    def addClientInfo(self, mailblenderGui):
+        firstName = self.FirstNameInput.text()
+        lastName = self.LastNameInput.text()
+        companyName = self.CompanyNameInput.text()
+        address1 = self.Address1Input.text()
+        address2 = self.Address2Input.text()
+        city = self.CityInput.text()
+        zip = self.ZipInput.text()
+        state = self.StateComboBox.currentText()
+        email = self.EmailInput.text()
+        phone = self.phoneFormat(self.PhoneInput.text())
+        blog = self.BlogURLInput.text()
+        homeValue = self.HomeValueURLInput.text()
+        homeSearch = self.HomeSearchURLInput.text()
+        hexColor = self.HexColorInput.text()
+
+        print(self.clientinfo)
+        if len(zip) != 5 and zip.isnumeric() == False:
+            print('Please enter a proper zip code')
+        elif len(zip) == 5 and zip.isnumeric() == True:
+            print(zip)
+
+        if hexColor.startswith('#') and len(hexColor) == 7:
+            self.HexColorFrame.setStyleSheet("background-color:" + hexColor + ";")
+            print(hexColor)
+
+        else:
+            hexColor = '#'+hexColor
+            self.HexColorFrame.setStyleSheet("background-color:" + hexColor + ";")
+            print(hexColor)
+
+        if state == 'State':
+            print('Please select a State')
+
+        client = self.clientinfo
+        name = firstName + ' ' + lastName
+        print(name)
+        client[name] = {
+            'firstName': firstName,
+            'lastName': lastName,
+            'companyName': companyName,
+            'address1': address1,
+            'address2': address2,
+            'city': city,
+            'zip': zip,
+            'state': state,
+            'email': email,
+            'phone': phone,
+            'blog': blog,
+            'homeValue': homeValue,
+            'homeSearch': homeSearch,
+            'hexColor': hexColor
+            }
+
+        self.write()
+        self.clientListWidget.addItem(client)
+        print(client)
+
+
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    MailBlender = QtWidgets.QMainWindow()
+    ui = Ui_MailBlender()
+    ui.setupUi(MailBlender)
+    prog = mailblenderGui(MailBlender)
+    MailBlender.show()
+    sys.exit(app.exec_())
